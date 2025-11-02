eacb8a67fcb61a6bc6125f280def69e84423d97b
Jgt <jagatkurnia14@gmail.com>
2025-11-02 21:52:17 +0700

    Remove commented-out code in penjumlahan.py

--- pertemuan/temu-3/penjumlahan.py
+++ pertemuan/temu-3/penjumlahan.py
@@ -1,4 +1,4 @@
-'''nilai1 = int(input("Masukan Nilai Pertama :"))
+nilai1 = int(input("Masukan Nilai Pertama :"))
 nilai2 = int(input("Masukan Nilai kedua :"))
 pemjumlahan = nilai1 + nilai2
 print(nilai1, "+", nilai2, "= ", pemjumlahan)
@@ -12,22 +12,6 @@ pemangkatan = nilai1 ** nilai2
 print(nilai1, "**", nilai2, "= ", pemangkatan)
 pembagian_bulat = nilai1 // nilai2
 print(nilai1, "//", nilai2, "= ", pembagian_bulat)
-'''
 
-'''n1 = int(input("nilai 1 : "))
-n2 = int(input("nilai 2 : ")) 
 
-print(n1 > n2)
-print(n1 < n2)
-print(n1 >= n2)
-print(n1 <= n2)
-print(n1 == n2)
-print(n1 != n2)'''
 
-#contoh bilangan binner
-print(bin(1))
-print(bin(7))
-
-print(1 & 7 )
-print(1 | 7 )
-print(1 ^ 7 )
