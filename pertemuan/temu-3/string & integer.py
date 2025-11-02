6223ad00013c0a44bfac07026a54ef40280cfb85
Jgt <jagatkurnia14@gmail.com>
2025-11-02 21:49:34 +0700

    Create string & integer.py

---
+++ pertemuan/temu-3/string & integer.py
@@ -0,0 +1,16 @@
+# program menghitung total bayar barang yang dijual
+
+# Input
+barang=input("Masukan Nama Barang :")
+harga=int(input("Masukan Harga Barang :"))
+jumlahjual=int(input("Masukan Jumlah Jual :"))
+
+# Proses
+totalharga=harga*jumlahjual
+
+# Output
+print("==================================")
+print("Nama Barang      :",barang)
+print("Total Barang     :", jumlahjual)
+print("Total Bayar      :",totalharga)
+print("==================================")
