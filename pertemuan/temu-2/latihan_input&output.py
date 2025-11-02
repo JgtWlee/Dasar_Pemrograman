468894ba038d281d90ba662ead2e545130431b28
Jgt <jagatkurnia14@gmail.com>
2025-11-02 21:53:26 +0700

    Update latihan_input&output.py

--- pertemuan/temu-2/latihan_input&output.py
+++ pertemuan/temu-2/latihan_input&output.py
@@ -1,4 +1,4 @@
-"""nama_pembeli=input("Masukan Nama Anda :")
+nama_pembeli=input("Masukan Nama Anda :")
 Kode_mainan=input("Masukan Kode Mainan :")
 jumlah_harga=input("Masukan Harga:")
 jumlah_beli=input("Masukan Jumlah Beli :")
@@ -10,21 +10,7 @@ print("Nama Pembeli =", nama_pembeli)
 print("Kode Mainan  =", Kode_mainan)
 print("Harga        =", jumlah_harga)
 print("Jumlah Beli  =", jumlah_beli)
-print("Total        =", total)"""
+print("Total        =", total)
 
-'''var2= "Bawaktehellwak"
-print(var2[2:6]) # wakt
-'''
-'''var1= 'Hello Python'
-var2= var1[:6]
-print(var1)
-print("String Update : - ", var1[:6]+'World') # String Update : -  Hello World
-print(var2)
-'''
 
-str1 = 'Hello'
-str2 = 'Python'
 
-print('str1 + str2 =', str1 +' '+ str2)
-print('str1 * 3 =', str1 * 3)
-print(len(str1))
